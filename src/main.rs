0634b4a616525bed15bda7c1db436585261a9dc7
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-07-24 16:07:10 +0200

    Changed cdmark to qcr

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 
     match dirs::data_local_dir() {
         Some(local_dir) => {
-            let paths_file_path = local_dir.join("cdmark/path.json");
+            let paths_file_path = local_dir.join("quicksave-cli-rs/path.json");
             let mut application = application::Application::new(std::env::args().collect(), &paths_file_path);
             match application.run() {
                 Ok(sstring) => {
